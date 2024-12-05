46196d2ec2342beddae3357f2ad5a63a0b01c42d
Ebuube <onwutaebubegideon1555@gmail.com>
2024-12-05 00:27:04 +0000

    Added Viewsets and Routers
    Reducing redundancy in endpoint patterns

--- posts/views.py
+++ posts/views.py
@@ -1,28 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import viewsets
 
 from .models import Post
 from .permissions import IsAuthOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthOrReadOnly,)
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serialzer_class = PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
